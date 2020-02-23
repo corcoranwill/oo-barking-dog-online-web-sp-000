0c53a211d8ee89c43f5be7c4d47a4fecf2962996
Will Corcoran <corcoranwill@gmail.com>
2020-02-23 01:08:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,12 @@
 # Your code goes here!
 class Dog
-  def name=(dogs_name)
-      @this_dogs_name = dogs_name
+  def @name name
+    name = name
+  end
+  def name
+    name
   end
   def bark
     puts "woof!"
   end
 end
-
-fido = Dog.new
-fido.name = "Fido"
-
-# fido.name
-#
-# fido.bark
